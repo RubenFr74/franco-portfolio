4d1b8d2e73789f7162e8658a81a201c2d7a7eebc
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 13:48:35 -0700

    added styling to Projects header

--- my-app/src/components/Projects.jsx
+++ my-app/src/components/Projects.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#023262]'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-[#f99709]'>Projects</p>
+                <p className='py-6'>Check out some of my recent work</p>
+            </div>
+            {/* Grid Container */}
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
