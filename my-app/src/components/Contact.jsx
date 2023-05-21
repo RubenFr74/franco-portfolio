b29794ef88c8330936ddd865228e245383134960
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 16:50:11 -0700

    added contact header and styling

--- my-app/src/components/Contact.jsx
+++ my-app/src/components/Contact.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#023262] flex justify-center items-center p-4'>
-
+        <form action='' className='flex flex-col max-w-[600px] w-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-[#f99709] text-gray-300'>Contact</p>
+            </div>
+        </form>
     </div>
   )
 }
