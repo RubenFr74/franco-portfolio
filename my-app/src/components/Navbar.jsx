57cc8961e5b56e3578713872fa8ba182f70f48e1
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 07:57:26 -0700

    started editing navbar.js

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from "../assets/logo2.png"
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#023262] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Image" style={{width:"50px"}}/>
+        </div>
+        <div>
+            <ul>
+                <li>About Me</li>
+                <li>Portfolio</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
     </div>
   )
 }
