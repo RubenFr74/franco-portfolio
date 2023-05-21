8cb73fc8c2cf597dffbb5a3bb4c8a6fffd4d9e77
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 13:32:46 -0700

    small refactoring

--- my-app/src/components/Skills.jsx
+++ my-app/src/components/Skills.jsx
@@ -9,9 +9,9 @@ import Tailwind from '../assets/tailwind.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#023262] text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-[#023262] text-gray-300'>
         {/* Container */}
-        <div className='max-w-[1000px] max-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-[#f99709]'>Experience</p>
                 <p className='py-4'>// These are the technology I'm experienced with.</p>
@@ -23,23 +23,23 @@ const Skills = () => {
                     <p>HTML</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={CSS} alt='HTML icon' />
+                    <img className='w-20 mx-auto' src={CSS} alt='CSS icon' />
                     <p>CSS</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={JavaScript} alt='HTML icon' />
+                    <img className='w-20 mx-auto' src={JavaScript} alt='JS icon' />
                     <p>JavaScript</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={ReactImg} alt='HTML icon' />
+                    <img className='w-20 mx-auto' src={ReactImg} alt='React icon' />
                     <p>React</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={GitHub} alt='HTML icon' />
+                    <img className='w-20 mx-auto' src={GitHub} alt='GitHub icon' />
                     <p>GitHub</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={Tailwind} alt='HTML icon' />
+                    <img className='w-20 mx-auto' src={Tailwind} alt='Tailwind icon' />
                     <p>Tailwind</p>
                 </div>
             </div>
