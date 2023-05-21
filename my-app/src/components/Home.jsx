ef8b77ff98eb6ebd8109c6f382c55af52ada2c05
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 11:52:00 -0700

    added styling to about me

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
 
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p>Hi, my name is</p>
-            <h1>Ruben Franco</h1>
-            <h2>I'm a Full Stack Developer.</h2>
-            <p>I’m a full-stack developer specializing in building (and occasionally
+            <p className='text-[#f99709]'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Ruben Franco</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I'm focused on
           building responsive full-stack web applications.</p>
           <div>
