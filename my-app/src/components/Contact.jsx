d61533aba934520b89e2976ff208c2297d7f8dfb
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 16:45:03 -0700

    created contact file

---
+++ my-app/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#023262] flex justify-center items-center p-4'>
+
+    </div>
+  )
+}
+
+export default Contact
