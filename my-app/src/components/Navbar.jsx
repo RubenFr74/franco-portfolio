9f97de717d5b2de41b3f6a848aa8c849f1d3e604
Ruben Franco <ruben.afranco@yahoo.com>
2023-05-21 08:34:00 -0700

    added styling to menu elements

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from "../assets/logo2.png"
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#023262] text-gray-300'>
         <div>
@@ -10,8 +14,8 @@ const Navbar = () => {
         </div>
 
         {/* Menu */}
-        <div>
-            <ul className='flex'>
+        <div className='hidden md:flex'>
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About Me</li>
                 <li>Portfolio</li>
@@ -20,16 +24,16 @@ const Navbar = () => {
             </ul>
         </div>
         {/* Hamburger Menu */}
-        <div className='hidden'>
+        <div onClick={handleClick} className='md:hidden z-10'>
           <FaBars />
         </div>
         {/* Mobile Menu */}
-        <ul className='hidden'>
-          <li>Home</li>
-          <li>About Me</li>
-          <li>Portfolio</li>
-          <li>Resume</li>
-          <li>Contact</li>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#023262] flex flex-col justify-center items-center'}>
+          <li className='py-6 text-4xl'>Home</li>
+          <li className='py-6 text-4xl'>About Me</li>
+          <li className='py-6 text-4xl'>Portfolio</li>
+          <li className='py-6 text-4xl'>Resume</li>
+          <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
 
